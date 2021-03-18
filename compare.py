2efe1f290345b871c7f5ccf688d2048a03c03b98
Drewbty <80837965+Drewbty@users.noreply.github.com>
2021-03-19 00:15:11 +0500

    updates compare file commited

--- compare.py
+++ compare.py
@@ -19,41 +19,51 @@ class compare_code:
             other_not_black = []
 
             #matching and finding the correct positions and colour
-            for i, peg in enumerate(code):
-                if peg == guess[i]:
-                    blacks_count += 1
+            try:
+                    
+                for i, peg in enumerate(code):
+                    try:
+                        if peg == guess[i]:
+                            blacks_count += 1
+                        else:
+                            self_not_black.append(peg)
+                            other_not_black.append(guess[i])
+                    except:
+                        self_not_black.append(peg)
+                        other_not_black.append(guess[i])
+
+
+
+                self_not_black_counter = Counter(self_not_black)
+                other_not_black_counter = Counter(other_not_black)
+
+                for color, count in self_not_black_counter.items():
+                    whites_count += min(count, other_not_black_counter[color])
+
+                # making the list of blacks and whites    
+                out = []
+                while blacks_count>0:
+                    out.append('B')
+                    blacks_count -=1
+                while whites_count>0:
+                    out.append('W')
+                    whites_count -=1
+                if(len(out) >= 1):
+                    self.feedbackReturn=out
                 else:
-                    self_not_black.append(peg)
-                    other_not_black.append(guess[i])
-
-            self_not_black_counter = Counter(self_not_black)
-            other_not_black_counter = Counter(other_not_black)
-
-            for color, count in self_not_black_counter.items():
-                whites_count += min(count, other_not_black_counter[color])
-
-            # making the list of blacks and whites    
-            out = []
-            while blacks_count>0:
-                out.append('B')
-                blacks_count -=1
-            while whites_count>0:
-                out.append('W')
-                whites_count -=1
-            if(len(out) >= 1):
-                self.feedbackReturn=out
-            else:
-                self.feedbackReturn="Nothing"
+                    self.feedbackReturn="Nothing"
+            except:
+                self.feedbackReturn ='Nothingg'
         
 
         
 # to get the players
-class player:
+class Player:
     '''Contains the player info'''
     
-    p = [None]*4
+    Playerlist = [None]*4
     def __init__(self, n):
-        for i in range(n):
-            print("Player ", i+1)
-            player.p[i]= input(" What is your name? \n >")
+        for index in range(n):
+            temp = index+1
+            Player.Playerlist[index]= input("Player " +temp+": What is your name?  >")
 
